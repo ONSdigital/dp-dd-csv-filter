304b3bca5000d68343ddd2068268dfc7ff118a6c
Matt Smith <msmith@equalexperts.com>
2017-02-20 11:27:26 +0000

    Modified the filterer to write to an intermediate s3 bucket (sub-folder) and send a kafka message to the transformer

--- handlers/handler_test.go
+++ handlers/handler_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ONSdigital/dp-dd-csv-filter/aws"
 	"github.com/ONSdigital/dp-dd-csv-filter/message/event"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/Shopify/sarama"
 )
 
 var mutex = &sync.Mutex{}
@@ -27,6 +28,7 @@ type MockAWSCli struct {
 	fileBytes      []byte
 	err            error
 }
+
 
 func newMockAwsClient() *MockAWSCli {
 	mock := &MockAWSCli{requestedFiles: make(map[string]int), savedFiles: make(map[string]int)}
@@ -88,14 +90,47 @@ func (p *MockCSVProcessor) Process(r io.Reader, w io.Writer, d map[string][]stri
 	}
 }
 
+// MockProducer
+type MockProducer struct {
+	sentMessages []string
+	sendMessageError error
+	sendMessagesInvocations int
+	messageTopics []string
+}
+
+func newMockProducer() *MockProducer {
+	mock := MockProducer{}
+	return &mock
+}
+
+func (p *MockProducer) SendMessage(msg *sarama.ProducerMessage) (partition int32, offset int64, err error) {
+	messageText, _ := msg.Value.Encode()
+	p.sentMessages = append(p.sentMessages, string(messageText))
+	p.messageTopics = append(p.messageTopics, msg.Topic)
+	return 0, 0, p.sendMessageError
+}
+
+func (p *MockProducer) SendMessages(msgs []*sarama.ProducerMessage) error {
+	p.sendMessagesInvocations++
+	return errors.New("Should not be calling SendMessages!")
+}
+
+func (p *MockProducer) Close() error {
+	return nil
+}
+
+var filterBucket = "filter-bucket"
+var topicName = "transform-topic"
+
 func TestHandler(t *testing.T) {
 
-	Convey("Should invoke AWSClient once with the request file path.", t, func() {
+	Convey("Should invoke AWSClient once with the filter file path.", t, func() {
 		recorder := httptest.NewRecorder()
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 
-		inputFile := "s3://bucket/test.csv"
-		outputFile := "s3://bucket/test.out"
+		inputFile := "s3://input-bucket/test.csv"
+		outputFile := "s3://transform-bucket/test.out"
+		filterFile := "s3://filter-bucket/test.out"
 		dimensions := map[string][]string{"dim": {"foo"}}
 		filterRequest := createFilterRequest(inputFile, outputFile, dimensions)
 
@@ -107,13 +142,18 @@ func TestHandler(t *testing.T) {
 		So(status, ShouldResemble, http.StatusOK)
 		So(1, ShouldEqual, mockAWSCli.getTotalInvocations())
 		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(inputFile))
-		So(1, ShouldEqual, mockAWSCli.countOfSaveInvocations(outputFile))
+		So(1, ShouldEqual, mockAWSCli.countOfSaveInvocations(filterFile))
+		So(0, ShouldEqual, mockAWSCli.countOfSaveInvocations(outputFile))
 		So(1, ShouldEqual, mockCSVProcessor.invocations)
+		So(1, ShouldEqual, len(mockProducer.sentMessages))
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, filterFile)
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, outputFile)
+		So(mockProducer.messageTopics[0], ShouldEqual, topicName)
 	})
 
 	Convey("Should return appropriate error if cannot unmarshall the request body into a FilterRequest.", t, func() {
 		recorder := httptest.NewRecorder()
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 
 		Handle(recorder, createRequest("This is not a FilterRequest"))
 
@@ -121,6 +161,7 @@ func TestHandler(t *testing.T) {
 
 		So(0, ShouldEqual, mockAWSCli.getTotalInvocations())
 		So(0, ShouldEqual, mockCSVProcessor.invocations)
+		So(0, ShouldEqual, len(mockProducer.sentMessages))
 		So(splitterResponse, ShouldResemble, filterRespUnmarshalBody)
 		So(status, ShouldResemble, http.StatusBadRequest)
 	})
@@ -130,7 +171,7 @@ func TestHandler(t *testing.T) {
 		uri := "s3://bucket/target.csv"
 		awsErrMsg := "THIS IS AN AWS ERROR"
 
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 		mockAWSCli.err = errors.New(awsErrMsg)
 
 		Handle(recorder, createRequest(createFilterRequest(uri, uri, nil)))
@@ -139,22 +180,58 @@ func TestHandler(t *testing.T) {
 		So(1, ShouldEqual, mockAWSCli.getTotalInvocations())
 		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(uri))
 		So(0, ShouldEqual, mockCSVProcessor.invocations)
+		So(0, ShouldEqual, len(mockProducer.sentMessages))
 		So(splitterResponse, ShouldResemble, FilterResponse{awsErrMsg})
 		So(status, ShouldResemble, http.StatusBadRequest)
 	})
 
 	Convey("Should return success response for happy path scenario", t, func() {
 		recorder := httptest.NewRecorder()
-		uri := "s3://bucket/target.csv"
+		inputUri := "s3://input-bucket/target.csv"
+		outputUri := "s3://output-bucket/target.csv"
+		filterUri := "s3://filter-bucket/target.csv"
 
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 
-		Handle(recorder, createRequest(createFilterRequest(uri, uri, nil)))
+		Handle(recorder, createRequest(createFilterRequest(inputUri, outputUri, nil)))
 		splitterResponse, statusCode := extractResponseBody(recorder)
 
 		So(1, ShouldEqual, mockAWSCli.getTotalInvocations())
-		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(uri))
+		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(inputUri))
 		So(1, ShouldEqual, mockCSVProcessor.invocations)
+		So(1, ShouldEqual, len(mockProducer.sentMessages))
+		So(1, ShouldEqual, mockAWSCli.countOfSaveInvocations(filterUri))
+		So(0, ShouldEqual, mockAWSCli.countOfSaveInvocations(outputUri))
+		So(1, ShouldEqual, len(mockProducer.sentMessages))
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, filterUri)
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, outputUri)
+		So(mockProducer.messageTopics[0], ShouldEqual, topicName)
+		So(splitterResponse, ShouldResemble, filterResponseSuccess)
+		So(statusCode, ShouldResemble, http.StatusOK)
+	})
+
+	Convey("Should handle a bucket path with or without a trailing slash", t, func() {
+		recorder := httptest.NewRecorder()
+		inputUri := "s3://input-bucket/target.csv"
+		outputUri := "s3://output-bucket/target.csv"
+		filterUri := "s3://filter-bucket/target.csv"
+
+		setOutputS3Bucket("filter-bucket/")
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
+
+		Handle(recorder, createRequest(createFilterRequest(inputUri, outputUri, nil)))
+		splitterResponse, statusCode := extractResponseBody(recorder)
+
+		So(1, ShouldEqual, mockAWSCli.getTotalInvocations())
+		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(inputUri))
+		So(1, ShouldEqual, mockCSVProcessor.invocations)
+		So(1, ShouldEqual, len(mockProducer.sentMessages))
+		So(1, ShouldEqual, mockAWSCli.countOfSaveInvocations(filterUri))
+		So(0, ShouldEqual, mockAWSCli.countOfSaveInvocations(outputUri))
+		So(1, ShouldEqual, len(mockProducer.sentMessages))
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, filterUri)
+		So(mockProducer.sentMessages[0], ShouldContainSubstring, outputUri)
+		So(mockProducer.messageTopics[0], ShouldEqual, topicName)
 		So(splitterResponse, ShouldResemble, filterResponseSuccess)
 		So(statusCode, ShouldResemble, http.StatusOK)
 	})
@@ -163,20 +240,21 @@ func TestHandler(t *testing.T) {
 		recorder := httptest.NewRecorder()
 		uri := "s3://bucket/unsupported.txt"
 
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 
 		Handle(recorder, createRequest(createFilterRequest(uri, uri, nil)))
 
 		splitterResponse, status := extractResponseBody(recorder)
 		So(0, ShouldEqual, mockAWSCli.getTotalInvocations())
 		So(0, ShouldEqual, mockCSVProcessor.invocations)
+		So(0, ShouldEqual, len(mockProducer.sentMessages))
 		So(splitterResponse, ShouldResemble, filterRespUnsupportedFileType)
 		So(status, ShouldResemble, http.StatusBadRequest)
 	})
 
 	Convey("Should handle a panic.", t, func() {
 		recorder := httptest.NewRecorder()
-		mockAWSCli, mockCSVProcessor := setMocks(ioutil.ReadAll)
+		mockAWSCli, mockCSVProcessor, mockProducer := setMocks(ioutil.ReadAll)
 
 		inputFile := "s3://bucket/test.csv"
 		outputFile := "s3://bucket/test.out"
@@ -195,8 +273,39 @@ func TestHandler(t *testing.T) {
 		So(1, ShouldEqual, mockAWSCli.getInvocationsByURI(inputFile))
 		So(0, ShouldEqual, mockAWSCli.countOfSaveInvocations(outputFile))
 		So(1, ShouldEqual, mockCSVProcessor.invocations)
+		So(0, ShouldEqual, len(mockProducer.sentMessages))
 	})
 
+}
+
+func TestGetFilterS3Url(t *testing.T) {
+	outputUrl, _ := aws.NewS3URL("s3://output-bucket/folder/filename.csv")
+	Convey("Should return appropriate error if s3Url cannot be created.", t, func() {
+		setOutputS3Bucket("invalid s3 bucket")
+		_, err := getFilterS3Url(outputUrl)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Should return s3 url when bucket includes s3://", t, func() {
+		setOutputS3Bucket("s3://valid-bucket")
+		s3, err := getFilterS3Url(outputUrl)
+		So(err, ShouldBeNil)
+		result := s3.String()
+		So(result, ShouldEqual, "s3://valid-bucket/filename.csv")
+	})
+	Convey("Should return s3 url when bucket does not include s3://", t, func() {
+		setOutputS3Bucket("valid-bucket/")
+		s3, err := getFilterS3Url(outputUrl)
+		So(err, ShouldBeNil)
+		result := s3.String()
+		So(result, ShouldEqual, "s3://valid-bucket/filename.csv")
+	})
+	Convey("Should return s3 url when bucket includes path and trailing /", t, func() {
+		setOutputS3Bucket("valid-bucket/valid-folder/")
+		s3, err := getFilterS3Url(outputUrl)
+		So(err, ShouldBeNil)
+		result := s3.String()
+		So(result, ShouldEqual, "s3://valid-bucket/valid-folder/filename.csv")
+	})
 }
 
 func extractResponseBody(rec *httptest.ResponseRecorder) (FilterResponse, int) {
@@ -219,9 +328,13 @@ func createFilterRequest(input string, output string, dimensions map[string][]st
 	return req
 }
 
-func setMocks(reader requestBodyReader) (*MockAWSCli, *MockCSVProcessor) {
+func setMocks(reader requestBodyReader) (*MockAWSCli, *MockCSVProcessor, *MockProducer) {
 	mockAWSCli := newMockAwsClient()
 	mockCSVProcessor := newMockCSVProcessor()
+	mockProducer := newMockProducer()
+	SetProducer(mockProducer)
 	setReader(reader)
-	return mockAWSCli, mockCSVProcessor
+	setOutputS3Bucket(filterBucket)
+	setTransformTopic(topicName)
+	return mockAWSCli, mockCSVProcessor, mockProducer
 }
